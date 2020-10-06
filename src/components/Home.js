d2acc3daef17c09800c1d2485f55e49b3da36f94
Karma Drukpa <karmadrukpa11@gmail.com>
2020-10-05 23:47:56 -0400

    Add Google font and home page png image and style a bit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from '../images/front-end.png';
 import './Home.css';
 
 const Home = () => {
@@ -7,11 +8,13 @@ const Home = () => {
 			<div className='home__row'>
 				<div className='home__column'>
 					<div className='home__imageColumn'>
-						<img src='../images/' alt='' />
+						<img src={image} alt='home-pageImage' />
 					</div>
 				</div>
 				<div className='home__column'>
-					<div className='home__introColumn'>Hello Programmer</div>
+					<div className='home__introColumn'>
+						<h1>Hello, I am Karma, a Front end developer</h1>
+					</div>
 				</div>
 			</div>
 		</div>
