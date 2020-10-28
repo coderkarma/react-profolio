a3f0b33e389de13e44bb71a127fee5a9c01655ac
Karma Drukpa <karmadrukpa11@gmail.com>
2020-10-27 20:44:00 -0400

    Add svg image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import image from '../images/front-end.png';
+import image from '../images/developer.svg';
 import './Home.css';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<div className='home__row'>
-				<div className='home__imageColumn'>
-					<img src={image} alt='home-pageImage' />
-				</div>
+			<div className='home__imageColumn'>
+				<img src={image} alt='home-pageImage' />
+			</div>
 
-				<div className='home__introColumn'>
-					<h1>Hello, I am Karma, a Front end developer</h1>
-				</div>
+			<div className='home__introColumn'>
+				<h1>Hello, I'm Karma, a Front End Developer</h1>
 			</div>
 		</div>
 	);
